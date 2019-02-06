8a4c4181956b404880ceec7d194658f51b330007
Sabbu Uday Kumar <sabbuuday.suk@gmail.com>
2019-02-06 12:58:52 +0530

    Auth adapter security context, Mosip user updated
    Former-commit-id: 55cb57868f1a05c5e0637420a24cec6d96d963fc
    Former-commit-id: 3aaec50af5b621105dd71cdd11efdc971e7cf943
    Former-commit-id: fb235f583ab8c59444bab4c09e602f2c0f8c8af5

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/adapter/MosipUserDto.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/adapter/MosipUserDto.java
@@ -4,10 +4,13 @@ package io.mosip.kernel.auth.adapter;
  * MOSIP USER IS THE STANDARD SPEC THAT WILL BE TUNED BASED ON THE DETAILS STORED IN LDAP FOR A USER
  **********************************************************************************************************************/
 
-public class MosipUser {
+public class MosipUserDto {
     private String userName;
     private String mobile;
     private String mail;
+    private String langCode;
+    private byte[] userPassword;
+    private String name;
     private String role;
 
     public String getUserName() {
@@ -40,5 +43,29 @@ public class MosipUser {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getLangCode() {
+        return langCode;
+    }
+
+    public void setLangCode(String langCode) {
+        this.langCode = langCode;
+    }
+
+    public byte[] getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(byte[] userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
