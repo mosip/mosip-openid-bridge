a8c0f87e96d5d722a82751e8584871a83db62906
mahammedtaheer <57249563+mahammedtaheer@users.noreply.github.com>
2023-12-13 10:02:44 +0530

    Fixed test case failure. (#213)
    Signed-off-by: Mahammed Taheer <mohd.taheer@gmail.com>Signed-off-by: Ashok Kumar Sharma <ashok@mosip.io>

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/filter/AuthFilter.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/filter/AuthFilter.java
@@ -10,8 +10,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
