5ad56302903eeb8aa3fee6f16e6ac2969dca7e5f
Balaji Alluru <74903654+balaji-alluru@users.noreply.github.com>
2024-10-22 13:33:30 +0530

    [MOSIP-35847] Updated token helper (#282)
    * [MOSIP-35847] Updated token helper
    
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>
    
    * updated SelfTokenExchangeFilterFunction
    
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>Signed-off-by: Ashok Kumar Sharma <ashok@mosip.io>

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/SelfTokenExchangeFilterFunction.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/SelfTokenExchangeFilterFunction.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import io.mosip.kernel.auth.defaultadapter.constant.AuthAdapterConstant;
 import io.mosip.kernel.auth.defaultadapter.constant.AuthAdapterErrorCode;
