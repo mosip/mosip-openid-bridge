896d178f2d8c697f5562ceb5aba4f39fd700944b
kameshsr <kameshsr1338@gmail.com>
2025-02-10 16:50:42 +0530

    MOSIP-39225 corrected validation
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/helper/TokenValidationHelper.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/helper/TokenValidationHelper.java
@@ -78,15 +78,20 @@ public class TokenValidationHelper {
 
     private MosipUserDto doOfflineEnvTokenValidation(String jwtToken, RestTemplate restTemplate) {
 
-        DecodedJWT decodedJWT;
         try {
-            decodedJWT = JWT.decode(jwtToken);
+            // Ensure token is in correct format (header.payload.signature)
+            if (!jwtToken.contains(".") || jwtToken.split("\\.").length != 3) {
+                LOGGER.error("Invalid JWT format: {}", jwtToken);
+                throw new AuthManagerException(AuthAdapterErrorCode.UNAUTHORIZED.getErrorCode(),
+                        AuthAdapterErrorCode.UNAUTHORIZED.getErrorMessage());
+            }
         }catch (Exception e){
             LOGGER.error("JWT decode failure :{}",e.getMessage());
             throw new AuthManagerException(AuthAdapterErrorCode.UNAUTHORIZED.getErrorCode(),
                     AuthAdapterErrorCode.UNAUTHORIZED.getErrorMessage());
         }
 
+        DecodedJWT decodedJWT = JWT.decode(jwtToken);
         PublicKey publicKey = validateTokenHelper.getPublicKey(decodedJWT);
         // Still not able to get the public key either from server or local cache,
         // proceed with online token validation.
